1e428edacb57946914bd6ac3fb28f1b16829ff60
Tijani Fulani <Fulani07@gmail.com>
2020-08-31 12:42:15 +0100

    Add files via upload
    build files

---
+++ build/precache-manifest.4a44e4a23d1690a5dc51af38f65f4325.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6ba026593dcb7dec4300e918669a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c2ec4402ee9f79fc2d1",
+    "url": "/static/css/main.d0249ae8.chunk.css"
+  },
+  {
+    "revision": "d4cb241f281c97424db8",
+    "url": "/static/js/2.029bca11.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/static/js/2.029bca11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2ec4402ee9f79fc2d1",
+    "url": "/static/js/main.38cf7dfa.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "524d83e0001f2abe4c5466ad19b575a5",
+    "url": "/static/media/Tijani-Fulani-cv.524d83e0.pdf"
+  },
+  {
+    "revision": "64d7d20ee6ab8dc825928842db4a56d5",
+    "url": "/static/media/me-copy.64d7d20e.png"
+  }
+]);
